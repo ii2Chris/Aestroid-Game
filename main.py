725dcda1e7c31fe31d9ec10c82db79e318ea5c6a
ii2Chris <ChrisBudhram03@outlook.com>
2025-10-05 16:19:53 -0400

    Added user icon, added rotation methods

--- main.py
+++ main.py
@@ -3,21 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    number = 42
-    while number != 0:
+    clock = pygame.time.Clock()
+
+    x, y = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
+
+    player = Player(x, y, PLAYER_RADIUS)
+
+    running = True
+    while running:
+        dt = clock.get_time() / 1000  # delta time in seconds
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
+        player.update(dt)             # update player
 
 
+        screen.fill((0, 0, 0))        # clear screen
+        player.draw(screen)           # draw player
+        pygame.display.flip()         # show everything you drew
+        clock.tick(60)                # limit to 60 FPS
 
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
